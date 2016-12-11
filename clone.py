dcd5165b0c7769e5830040717f0d9d595fa7b61c
Evgeniya Tokarchuk <evgeniya@tokarch.uk>
2016-12-11 23:23:46 +0500

    check url on uniqueness; process url

--- clone.py
+++ clone.py
@@ -18,8 +18,6 @@ import re
 import os
 import sys
 
-from urllib.parse import urlparse, unquote
-
 import asyncio
 from asyncio import Queue
 import argparse
@@ -32,7 +30,7 @@ from bs4 import BeautifulSoup
 class Cloner(object):
     def __init__(self, root):
         self.visited_urls = []
-        self.root = self.make_abs_url(root)
+        self.root = self.add_scheme(root)
         if len(self.root.host) < 4:
             sys.exit('invalid taget {}'.format(self.root.host))
         self.target_path = '/opt/snare/pages/{}'.format(self.root.host)
@@ -42,65 +40,48 @@ class Cloner(object):
 
         self.new_urls = Queue()
 
-    def make_abs_url(self, url):
-        if yarl.URL(url).is_absolute():
-            root = yarl.URL(url)
+    @staticmethod
+    def add_scheme(url):
+        if yarl.URL(url).scheme:
+            new_url = yarl.URL(url)
         else:
-            root = yarl.URL('http://' + url)
-        return root
+            new_url = yarl.URL('http://' + url)
+        return new_url
 
-    def make_new_link(self, url, abs):
-        if url.path == '/':
-            url.joun(yarl.URL('index.html'))
-        if not abs:
-            url = url.relative()
-        else:
-            url = url.with_host(self.root.host)
-        return url.human_repr()
+    @asyncio.coroutine
+    def process_link(self, url, check_host=False):
+        url = yarl.URL(url)
+        if check_host:
+            if (url.host != self.root.host or url.fragment
+                            or url in self.visited_urls):
+                return None
+        if not url.is_absolute():
+            url = self.root.join(url)
+
+        yield from self.new_urls.put(url)
+        return url.relative().human_repr()
 
     @asyncio.coroutine
     def replace_links(self, data):
         soup = BeautifulSoup(data, 'html.parser')
-        base_url = yarl.URL(self.root)
 
         # find all relative links
         for link in soup.findAll(href=True):
-            abs=False
-            url = yarl.URL(link['href'])
-            if url.is_absolute():
-                abs=True
-            else:
-                url = self.root.join(url)
-            if (url.host != self.root.host or url.fragment or
-                        url in self.visited_urls):
-                continue
-
-            yield from self.new_urls.put(url)
-            link['href'] = url.relative()
+            res = yield from self.process_link(link['href'], check_host=True)
+            if res is not None:
+                link['href'] = res
 
         # find all images and scripts
         for elem in soup.findAll(src=True):
-            abs = False
-            url = yarl.URL(elem['src'])
-            if url.is_absolute():
-                abs = True
-            else:
-                url = self.root.join(url)
-
-            yield from self.new_urls.put(url)
-            elem['src']=url.relative()
+            res = yield from self.process_link(elem['src'])
+            if res is not None:
+                elem['src'] = res
 
         # find all action elements
         for act_link in soup.findAll(action=True):
-            abs = False
-            url = yarl.URL(act_link['action'])
-            if url.is_absolute():
-                abs = True
-            else:
-                url = self.root.join(url)
-
-            yield from self.new_urls.put(url)
-            act_link['action'] = url.relative()
+            res = yield from self.process_link(act_link['action'])
+            if res is not None:
+                act_link['action'] = res
 
         # prevent redirects
         for redir in soup.findAll(True, attrs={'name': re.compile('redirect.*')}):
@@ -112,8 +93,15 @@ class Cloner(object):
     def get_body(self):
         while not self.new_urls.empty():
             current_url = yield from self.new_urls.get()
+            if current_url in self.visited_urls:
+                continue
             self.visited_urls.append(current_url)
-            file_name = current_url.name if current_url.name else 'index.html'
+            if current_url.name:
+                file_name = current_url.name
+            elif current_url.raw_path != '/':
+                file_name = current_url.path.rsplit('/')[1]
+            else:
+                file_name = 'index.html'
             file_path = os.path.dirname(current_url.path)
             if file_path == '/':
                 file_path = self.target_path
@@ -131,8 +119,8 @@ class Cloner(object):
                     with aiohttp.ClientSession() as session:
                         response = yield from session.get(current_url)
                         data = yield from response.read()
-            except Exception as e:
-                print(e)
+            except aiohttp.ClientError as client_error:
+                print(client_error)
             else:
                 response.release()
                 session.close()
@@ -171,8 +159,8 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--target", help="domain of the page to be cloned", required=True)
     args = parser.parse_args()
-    c = Cloner(args.target)
-    loop.run_until_complete(c.run())
+    cloner = Cloner(args.target)
+    loop.run_until_complete(cloner.run())
 
 
 if __name__ == '__main__':
