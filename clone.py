13cd31e3fc2b277486c24cdccb33848b3eda333e
Evgeniya <vremendm@gmail.com>
2016-12-12 02:48:19 +0500

    Feature/new cloner (#41)
    * new cloner
    
    * check url on uniqueness; process url
    
    * replace redirect value with string, not URL

--- clone.py
+++ clone.py
@@ -18,139 +18,133 @@ import re
 import os
 import sys
 
-from urllib.parse import urlparse, unquote
-
 import asyncio
+from asyncio import Queue
 import argparse
 import aiohttp
 import cssutils
+import yarl
 from bs4 import BeautifulSoup
 
 
 class Cloner(object):
+    def __init__(self, root):
+        self.visited_urls = []
+        self.root = self.add_scheme(root)
+        if len(self.root.host) < 4:
+            sys.exit('invalid taget {}'.format(self.root.host))
+        self.target_path = '/opt/snare/pages/{}'.format(self.root.host)
+
+        if not os.path.exists(self.target_path):
+            os.mkdir(self.target_path)
+
+        self.new_urls = Queue()
+
     @staticmethod
-    def make_new_link(url):
-        unquote(url)
-        parsed = urlparse(url)
-
-        if parsed.path[1:]:
-            new_link = parsed.path
-        elif parsed.query:
-            new_link = '/' + parsed.query
+    def add_scheme(url):
+        if yarl.URL(url).scheme:
+            new_url = yarl.URL(url)
         else:
-            new_link = '/index'
-        if new_link[-1] == '/':
-            new_link = new_link[:-1]
-        if '.' not in new_link:
-            new_link += '.html'
-        if parsed.fragment:
-            new_link += '/#' + parsed.fragment
-        if parsed.query:
-            new_link += '?' + parsed.query
+            new_url = yarl.URL('http://' + url)
+        return new_url
 
-        return new_link
+    @asyncio.coroutine
+    def process_link(self, url, check_host=False):
+        url = yarl.URL(url)
+        if check_host:
+            if (url.host != self.root.host or url.fragment
+                            or url in self.visited_urls):
+                return None
+        if not url.is_absolute():
+            url = self.root.join(url)
 
-    def replace_links(self, data, domain, urls):
+        yield from self.new_urls.put(url)
+        return url.relative().human_repr()
+
+    @asyncio.coroutine
+    def replace_links(self, data):
         soup = BeautifulSoup(data, 'html.parser')
-        patt = '.*' + domain + '.*'
 
         # find all relative links
-        for link in soup.findAll(True, attrs={'href': re.compile('^((?!http|\/\/|\.\.).)*$')}):
-            if link['href'].startswith('/'):
-                link['href'] = link['href'][1:]
-            abs_link = 'http://' + domain + link['href']
-            urls.append(abs_link)
-
-        # find all absolute links
-        for link in soup.findAll(True, attrs={'href': re.compile(patt)}):
-            urls.append(link['href'])
-            link['href'] = self.make_new_link(link['href'])
+        for link in soup.findAll(href=True):
+            res = yield from self.process_link(link['href'], check_host=True)
+            if res is not None:
+                link['href'] = res
 
         # find all images and scripts
-        for elem in soup.findAll(True, attrs={'src': re.compile('^((?!http|\/\/|\.\.).)*$')}):
-            abs_link = 'http://' + domain + elem['src']
-            urls.append(abs_link)
+        for elem in soup.findAll(src=True):
+            res = yield from self.process_link(elem['src'])
+            if res is not None:
+                elem['src'] = res
 
         # find all action elements
-        for act_link in soup.findAll(True, attrs={'action': re.compile(patt)}):
-            urls.append(act_link['action'])
-            act_link['action'] = self.make_new_link(act_link['action'])
+        for act_link in soup.findAll(action=True):
+            res = yield from self.process_link(act_link['action'])
+            if res is not None:
+                act_link['action'] = res
 
         # prevent redirects
         for redir in soup.findAll(True, attrs={'name': re.compile('redirect.*')}):
-            redir['value'] = self.make_new_link(redir['value'])
+            redir['value'] = yarl.URL(redir['value']).relative().human_repr()
 
-        urls = list(set(urls))
         return soup
 
     @asyncio.coroutine
-    def get_body(self, root_url, urls, visited_urls):
-        if not root_url.startswith("http"):
-            root_url = 'http://' + root_url
-        visited_urls.append(root_url)
-        parsed_url = urlparse(root_url)
-        if parsed_url.fragment:
-            return
-        domain = parsed_url.netloc
-        if not domain.endswith('/'):
-            domain += '/'
-        file_name = self.make_new_link(root_url)
-
-        file_path = ''
-        patt = '/.*/.*\.'
-        if re.match(patt, file_name):
-            file_path, file_name = file_name.rsplit('/', 1)
-            file_path += '/'
-        print('path: ', file_path, 'name: ', file_name)
-        if len(domain) < 4:
-            sys.exit('invalid taget {}'.format(root_url))
-        page_path = '/opt/snare/pages/{}'.format(domain)
-
-        if not os.path.exists(page_path):
-            os.mkdir(page_path)
-
-        if file_path and not os.path.exists(page_path + file_path):
-            os.makedirs(page_path + file_path)
-
-        data = None
-        try:
-            with aiohttp.Timeout(10.0):
-                with aiohttp.ClientSession() as session:
-                    response = yield from session.get(root_url)
-                    data = yield from response.read()
-        except Exception as e:
-            print(e)
-        else:
-            response.release()
-            session.close()
-        if data is not None:
-            if re.match(re.compile('.*\.(html|php)'), file_name):
-                soup = self.replace_links(data, domain, urls)
-                data = str(soup).encode()
-            with open(page_path + file_path + file_name, 'wb') as index_fh:
-                index_fh.write(data)
-            if '.css' in file_name:
-                css = cssutils.parseString(data)
-                for carved_url in cssutils.getUrls(css):
-                    if carved_url.startswith('data'):
-                        continue
-                    carved_url = os.path.normpath(os.path.join(domain, carved_url))
-                    if not carved_url.startswith('http'):
-                        if carved_url.startswith('..') or carved_url.startswith('/'):
-                            carved_url = 'http://' + domain + carved_url
-                        else:
-                            carved_url = 'http://' + carved_url
-                    if carved_url not in visited_urls:
-                        urls.insert(0, carved_url)
-        for url in urls:
-            urls.remove(url)
-            if url in visited_urls:
+    def get_body(self):
+        while not self.new_urls.empty():
+            current_url = yield from self.new_urls.get()
+            if current_url in self.visited_urls:
                 continue
-            yield from self.get_body(url, urls, visited_urls)
+            self.visited_urls.append(current_url)
+            if current_url.name:
+                file_name = current_url.name
+            elif current_url.raw_path != '/':
+                file_name = current_url.path.rsplit('/')[1]
+            else:
+                file_name = 'index.html'
+            file_path = os.path.dirname(current_url.path)
+            if file_path == '/':
+                file_path = self.target_path
+            else:
+                file_path = os.path.join(self.target_path, file_path[1:])
+
+            print('path: ', file_path, 'name: ', file_name)
+
+            if file_path and not os.path.exists(file_path):
+                os.makedirs(file_path)
+
+            data = None
+            try:
+                with aiohttp.Timeout(10.0):
+                    with aiohttp.ClientSession() as session:
+                        response = yield from session.get(current_url)
+                        data = yield from response.read()
+            except aiohttp.ClientError as client_error:
+                print(client_error)
+            else:
+                response.release()
+                session.close()
+            if data is not None:
+                if re.match(re.compile('.*\.(html|php)'), file_name):
+                    soup = yield from self.replace_links(data)
+                    data = str(soup).encode()
+                with open(os.path.join(file_path, file_name), 'wb') as index_fh:
+                    index_fh.write(data)
+                if '.css' in file_name:
+                    css = cssutils.parseString(data)
+                    for carved_url in cssutils.getUrls(css):
+                        if carved_url.startswith('data'):
+                            continue
+                        carved_url = yarl.URL(carved_url)
+                        if not carved_url.is_absolute():
+                            carved_url = self.root.join(carved_url)
+                        if carved_url not in self.visited_urls:
+                            yield from self.new_urls.put(carved_url)
 
     @asyncio.coroutine
-    def run(self, url):
-        return (yield from self.get_body(url, [], []))
+    def run(self):
+        yield from self.new_urls.put(self.root)
+        return (yield from self.get_body())
 
 
 def main():
@@ -165,8 +159,8 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--target", help="domain of the page to be cloned", required=True)
     args = parser.parse_args()
-    c = Cloner()
-    loop.run_until_complete(c.run(args.target))
+    cloner = Cloner(args.target)
+    loop.run_until_complete(cloner.run())
 
 
 if __name__ == '__main__':
