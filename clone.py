0c5abe97a74cdaa744f2645441020864f66bfc23
Evgeniya Tokarchuk <evgeniya@tokarch.uk>
2017-02-28 13:58:36 +0500

    check subdomains, create meta in case of error

--- clone.py
+++ clone.py
@@ -17,10 +17,10 @@ GNU General Public License for more details.
 import argparse
 import asyncio
 import hashlib
+import json
 import os
 import re
 import sys
-import json
 from asyncio import Queue
 
 import aiohttp
@@ -54,15 +54,30 @@ class Cloner(object):
     @asyncio.coroutine
     def process_link(self, url, check_host=False):
         url = yarl.URL(url)
-        if check_host:
-            if url.host != self.root.host or url.fragment:
-                return None
+        if url.scheme == ("data" or "javascript"):
+            return url.human_repr()
         if not url.is_absolute():
             url = self.root.join(url)
+        #get subdomains
+        host = url.host
+        if host is not None:
+            host_parts = host.split(".")
+
+            if len(host_parts) > 2:
+                host = host_parts[-2]+"."+host_parts[-1]
+
+        if check_host:
+            if host != self.root.host or url.fragment:
+                return None
 
         if url.human_repr() not in self.visited_urls:
             yield from self.new_urls.put(url)
-        return url.relative().human_repr()
+
+        try:
+            res = url.relative().human_repr()
+        except ValueError:
+            print(url)
+        return res
 
     @asyncio.coroutine
     def replace_links(self, data):
@@ -94,9 +109,13 @@ class Cloner(object):
         return soup
 
     def _make_filename(self, url):
+        host  = url.host
         file_name = url.relative().human_repr()
         if file_name == '/' or file_name == "":
-            file_name = 'index.html'
+            if host == self.root.host:
+                file_name = 'index.html'
+            else:
+                file_name = host
         m = hashlib.md5()
         m.update(file_name.encode('utf-8'))
         hash_name = m.hexdigest()
@@ -123,7 +142,7 @@ class Cloner(object):
             except (aiohttp.ClientError, asyncio.TimeoutError) as client_error:
                 print(client_error)
             else:
-                response.release()
+                yield from response.release()
             if data is not None:
                 self.meta[file_name]['hash'] = hash_name
                 self.meta[file_name]['content_type'] = content_type
@@ -146,11 +165,16 @@ class Cloner(object):
     @asyncio.coroutine
     def run(self):
         session = aiohttp.ClientSession()
-        yield from self.new_urls.put(self.root)
-        yield from self.get_body(session)
-        with open(os.path.join(self.target_path,'meta.json'), 'w') as mj:
-            json.dump(self.meta,mj)
-        session.close()
+        try:
+            yield from self.new_urls.put(self.root)
+            yield from self.get_body(session)
+        except KeyboardInterrupt:
+            raise
+        finally:
+            with open(os.path.join(self.target_path, 'meta.json'), 'w') as mj:
+                json.dump(self.meta, mj)
+            yield from session.close()
+
 
 def main():
     if os.getuid() != 0:
@@ -164,8 +188,11 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--target", help="domain of the page to be cloned", required=True)
     args = parser.parse_args()
-    cloner = Cloner(args.target)
-    loop.run_until_complete(cloner.run())
+    try:
+        cloner = Cloner(args.target)
+        loop.run_until_complete(cloner.run())
+    except KeyboardInterrupt:
+        pass
 
 
 if __name__ == '__main__':
