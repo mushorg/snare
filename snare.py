e548483daf19657a149495c85bd3c0866f9e0483
Ravinder Nehra <hnehra1@gmail.com>
2017-04-17 23:48:39 +0530

    fix #60 (#62)

--- snare.py
+++ snare.py
@@ -234,13 +234,17 @@ class HttpRequestHandler(aiohttp.server.ServerHttpProtocol):
                     self.writer, status=404, http_version=request.version
                 )
         response.add_header('Server', self.run_args.server_header)
+        
         if 'cookies' in data and 'sess_uuid' in data['cookies']:
             previous_sess_uuid = data['cookies']['sess_uuid']
         else:
             previous_sess_uuid = None
-        if previous_sess_uuid is None or not previous_sess_uuid.strip():
-            if 'sess_uuid' in event_result['response']['message']:
-                response.add_header('Set-Cookie', 'sess_uuid='+event_result['response']['message']['sess_uuid'])
+        
+        if 'sess_uuid' in event_result['response']['message']:
+            cur_sess_id = event_result['response']['message']['sess_uuid']
+            if previous_sess_uuid is None or not previous_sess_uuid.strip() or previous_sess_uuid != cur_sess_id:
+                response.add_header('Set-Cookie', 'sess_uuid=' + cur_sess_id)
+        
         if not content_type:
             response.add_header('Content-Type', 'text/plain')
         else:
