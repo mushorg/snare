42016190e42276b6b18a790ab5e3fc6e4421b2e3
surajt97 <suraj.t97@gmail.com>
2017-11-30 11:25:21 +0530

    PEP-8

--- logger.py
+++ logger.py
@@ -1,13 +1,14 @@
 import logging
 import logging.handlers
 
+
 class LevelFilter(logging.Filter):
     '''Filters (lets through) all messages with level < LEVEL'''
     def __init__(self, level):
         self.level = level
 
     def filter(self, record):
-        return record.levelno < self.level # "<" instead of "<=": since logger.setLevel is inclusive, this should be exclusive
+        return record.levelno < self.level  # "<" instead of "<=": since logger.setLevel is inclusive, this should be exclusive
 
 
 class Logger:
