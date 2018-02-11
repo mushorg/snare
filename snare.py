986ed6b36f1e6e89a34a080a85c5b95411345599
Viswak Hanumanth <viswakrocks@gmail.com>
2018-02-11 20:40:02 +0530

    Minor changes

--- snare.py
+++ snare.py
@@ -229,7 +229,7 @@ class HttpRequestHandler(aiohttp.server.ServerHttpProtocol):
                 requested_name = self.run_args.index_page
             try:
                 if requested_name[-1] == '/':
-                    requested_name = requested_name[0:-1]
+                    requested_name = requested_name[:-1]
                 requested_name = unquote(requested_name)
                 file_name = self.meta[requested_name]['hash']
                 content_type = self.meta[requested_name]['content_type']
