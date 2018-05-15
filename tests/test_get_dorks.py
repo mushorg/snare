35eabb0c7b4cdb12984726b99eb063ee46a543c1
viskey98 <viswakrocks@gmail.com>
2018-05-15 11:39:16 +0530

    Test get dorks

---
+++ tests/test_get_dorks.py
@@ -0,0 +1,35 @@
+import unittest
+import asyncio
+import argparse
+import aiohttp
+import shutil
+import os
+from snare import HttpRequestHandler
+
+
+class TestGetDorks(unittest.TestCase):
+    def setUp(self):
+        self.meta = {}
+        run_args = argparse.ArgumentParser();
+        run_args.add_argument("--tanner")
+        run_args.add_argument("--page-dir")
+        if not os.path.exists("/opt/snare/pages/test"):
+            os.mkdir("/opt/snare/pages/test")
+        self.args = run_args.parse_args(['--tanner', 'test'])
+        self.args = run_args.parse_args(['--page-dir', 'test'])
+        self.handler = HttpRequestHandler(self.meta, self.args)
+        self.data = []
+        self.loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(None)
+
+    def test_get_dorks(self):
+        self.handler.run_args.tanner = "tanner.mushmush.org"
+        try:
+            async def test():
+                self.data = await self.handler.get_dorks()
+            self.loop.run_until_complete(test())
+        except aiohttp.errors.ClientOSError:
+            self.fail("failed to get dorks from default tanner")
+
+    def tearDonw(self):
+        shutil.rmtree("/opt/snare/pages/test")
