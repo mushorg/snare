9875203b759c965367a55f8d01319b20b075efee
Viswak Hanumanth <viswakrocks@gmail.com>
2018-06-01 16:18:25 +0530

    Re-write cloner and move to setup structure (#141)
    * Re-writing snare with latest web server
    
    * Pep8 formatting
    
    * Breaking server.py
    
    * Break snare.py
    
    * Remove duplicates
    
    * Add port to server
    
    * Breaking server.py
    
    * Break snare.py
    
    * Remove duplicates
    
    * Improve project structure
    
    * Suggested changes
    
    * pep8 formatting
    
    * Suggested changes
    
    * rewrite and add clone to setup structure
    
    * Formatting
    
    * Formatting
    
    * Pep8 formatting

---
+++ snare/middlewares.py
@@ -0,0 +1,38 @@
+import aiohttp_jinja2
+from aiohttp import web
+
+
+class SnareMiddleware():
+
+    def __init__(self, file_name):
+        self.error_404 = file_name
+
+    async def handle_404(self, request):
+        return aiohttp_jinja2.render_template(self.error_404, request, {})
+
+    async def handle_500(self, request):
+        return aiohttp_jinja2.render_template('500.html', request, {})
+
+    def create_error_middleware(self, overrides):
+
+        @web.middleware
+        async def error_middleware(request, handler):
+            try:
+                response = await handler(request)
+                override = overrides.get(response.status)
+                if override:
+                    return await override(request)
+                return response
+            except web.HTTPException as ex:
+                override = overrides.get(ex.status)
+                if override:
+                    return await override(request)
+                raise
+        return error_middleware
+
+    def setup_middlewares(self, app):
+        error_middleware = self.create_error_middleware({
+            404: self.handle_404,
+            500: self.handle_500
+        })
+        app.middlewares.append(error_middleware)
