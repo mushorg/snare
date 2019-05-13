faa8ee1b4d6ac6e1d60b087ea9e474bd14e275cc
rjt-gupta <rajat.gupta99924@gmail.com>
2019-05-13 09:03:19 +0530

    updated changes

--- snare/tests/test_add_scheme.py
+++ snare/tests/test_add_scheme.py
@@ -9,7 +9,7 @@ from snare.utils.page_path_generator import generate_unique_path
 
 class TestCloner(unittest.TestCase):
     def setUp(self):
-        self.url = 'http://example.com/'
+        self.url = 'http://example.com'
         self.main_page_path = generate_unique_path()
         os.makedirs(self.main_page_path)
         self.expected_new_url = yarl.URL('http://example.com')
@@ -17,6 +17,12 @@ class TestCloner(unittest.TestCase):
         self.max_depth = sys.maxsize
         self.css_validate = 'false'
         self.handler = Cloner(self.url, self.max_depth, self.css_validate)
+
+    def test_trailing_slash(self):
+        self.url = 'http://example.com/'
+        new_url, err_url = self.handler.add_scheme(self.url)
+        self.assertEqual(new_url, self.expected_new_url)
+        self.assertEqual(err_url, self.expected_err_url)
 
     def test_add_scheme(self):
         new_url, err_url = self.handler.add_scheme(self.url)
