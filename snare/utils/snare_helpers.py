df68a3b72ad53d07a6661d249192d3c200089881
Parth Patil <Parthvin@gmail.com>
2020-03-07 02:52:51 +0530

    Fixed #256 -- TypeError in `print_color` (#258)
    handled the case if msg passed is not a string object to the `print_color` object due to which the return expression was throwing an error.

--- snare/utils/snare_helpers.py
+++ snare/utils/snare_helpers.py
@@ -129,4 +129,4 @@ def print_color(msg, mode='INFO'):
         color = colors[mode]
     except KeyError:
         color = colors['INFO']
-    print(color + msg + '\033[0m')
+    print(color + str(msg) + '\033[0m')
