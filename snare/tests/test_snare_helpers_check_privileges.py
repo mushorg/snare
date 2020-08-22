80cf37c291ef987206078bb7fc889152ee136409
krishna <krishnan_1901ee50@iitp.ac.in>
2020-08-22 10:47:41 +0530

    added more tests for check_privileges
    * the tests now cover the cases of non existent folders and their creation

--- snare/tests/test_snare_helpers_check_privileges.py
+++ snare/tests/test_snare_helpers_check_privileges.py
@@ -1,6 +1,6 @@
 import unittest
 from argparse import ArgumentTypeError
-from os.path import expanduser
+from os.path import expanduser, join
 
 from snare.utils.page_path_generator import generate_unique_path
 from snare.utils.snare_helpers import check_privileges
@@ -19,6 +19,24 @@ class TestStrToBool(unittest.TestCase):
 
     def test_privileges_in_home(self):
         self.path = expanduser('~')
+        try:
+            check_privileges(self.path)
+            self.privileges = True
+        except PermissionError:
+            self.privileges = False
+        assert self.privileges is True
+    
+    def test_non_existent_root_path(self):
+        self.path = '/snare'
+        try:
+            check_privileges(self.path)
+            self.privileges = True
+        except PermissionError:
+            self.privileges = False
+        assert self.privileges is False
+        
+    def test_non_existent_home_path(self):
+        self.path = join(expanduser('~'), 'snare')
         try:
             check_privileges(self.path)
             self.privileges = True
