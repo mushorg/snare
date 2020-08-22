7762b762b272f0599c16e11ef997c37d2899d33e
krishna <55460753+lordlabuckdas@users.noreply.github.com>
2020-08-22 12:33:31 +0530

    updated check_privileges (#275)
    * updated check_privileges
    
    * now, check_privileges will return an error with the location of conflicting folders
    
    * it also makes use of os.access to check if required permissions are satisfied
    
    * added more tests for check_privileges
    
    * the tests now cover the cases of non existent folders and their creation

--- snare/tests/test_snare_helpers_check_privileges.py
+++ snare/tests/test_snare_helpers_check_privileges.py
@@ -1,6 +1,6 @@
 import unittest
 from argparse import ArgumentTypeError
-from os.path import expanduser
+from os.path import expanduser, join
 
 from snare.utils.page_path_generator import generate_unique_path
 from snare.utils.snare_helpers import check_privileges
@@ -13,7 +13,7 @@ class TestStrToBool(unittest.TestCase):
         try:
             check_privileges(self.path)
             self.privileges = True
-        except IOError:
+        except PermissionError:
             self.privileges = False
         assert self.privileges is False
 
@@ -22,6 +22,24 @@ class TestStrToBool(unittest.TestCase):
         try:
             check_privileges(self.path)
             self.privileges = True
-        except IOError:
+        except PermissionError:
+            self.privileges = False
+        assert self.privileges is True
+    
+    def test_non_existent_root_path(self):
+        self.path = '/snare'
+        try:
+            check_privileges(self.path)
+            self.privileges = True
+        except PermissionError:
+            self.privileges = False
+        assert self.privileges is False
+        
+    def test_non_existent_home_path(self):
+        self.path = join(expanduser('~'), 'snare')
+        try:
+            check_privileges(self.path)
+            self.privileges = True
+        except PermissionError:
             self.privileges = False
         assert self.privileges is True
