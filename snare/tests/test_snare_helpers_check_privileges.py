6815dce0f297dad871becab561cb48c6e6f4713a
Lukas Rist <glaslos@gmail.com>
2021-06-12 11:27:40 +0200

    forgot how to format strings

--- snare/tests/test_snare_helpers_check_privileges.py
+++ snare/tests/test_snare_helpers_check_privileges.py
@@ -1,7 +1,6 @@
 import unittest
 from os.path import expanduser, join
 
-from snare.utils.page_path_generator import generate_unique_path
 from snare.utils.snare_helpers import check_privileges
 
 
@@ -11,25 +10,25 @@ class TestStrToBool(unittest.TestCase):
         try:
             check_privileges(self.path)
         except PermissionError as e:
-            self.fail("failed permissions check", e)
+            self.fail(f"failed permissions check{e}")
 
     def test_privileges_in_home(self):
         self.path = expanduser("~")
         try:
             check_privileges(self.path)
         except PermissionError as e:
-            self.fail("failed permissions check", e)
+            self.fail(f"failed permissions check{e}")
 
     def test_non_existent_root_path(self):
         self.path = "/snare"
         try:
             check_privileges(self.path)
         except PermissionError as e:
-            self.fail("failed permissions check", e)
+            self.fail(f"failed permissions check{e}")
 
     def test_non_existent_home_path(self):
         self.path = join(expanduser("~"), "snare")
         try:
             check_privileges(self.path)
         except PermissionError as e:
-            self.fail("failed permissions check", e)
+            self.fail(f"failed permissions check{e}")
