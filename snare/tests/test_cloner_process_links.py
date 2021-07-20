e7dc963e96298a17b10553fa707b7f55ab51575a
lordlabuckdas <55460753+lordlabuckdas@users.noreply.github.com>
2021-07-20 22:18:42 +0530

    test: process_links - update for try_count + basecloner

--- snare/tests/test_cloner_process_links.py
+++ snare/tests/test_cloner_process_links.py
@@ -3,7 +3,7 @@ import asyncio
 import sys
 import yarl
 from unittest import mock
-from snare.cloner import Cloner
+from snare.cloner import BaseCloner
 
 
 class TestProcessLinks(unittest.TestCase):
@@ -13,11 +13,12 @@ class TestProcessLinks(unittest.TestCase):
         self.max_depth = sys.maxsize
         self.loop = asyncio.new_event_loop()
         self.css_validate = False
-        self.handler = Cloner(self.root, self.max_depth, self.css_validate)
+        self.handler = BaseCloner(self.root, self.max_depth, self.css_validate)
         self.expected_content = None
         self.return_content = None
         self.return_url = None
         self.return_level = None
+        self.return_try_count = 0
         self.qsize = None
 
     def test_process_link_scheme(self):
@@ -28,6 +29,8 @@ class TestProcessLinks(unittest.TestCase):
         ]
 
         async def test(url_param):
+            if not self.handler:
+                raise Exception("Error initializing Cloner!")
             self.return_content = await self.handler.process_link(url_param, self.level)
             self.qsize = self.handler.new_urls.qsize()
 
@@ -44,13 +47,21 @@ class TestProcessLinks(unittest.TestCase):
         self.expected_content = "http://example.com/foo/путь/"
 
         async def test():
+            if not self.handler:
+                raise Exception("Error initializing Cloner!")
             self.return_content = await self.handler.process_link(self.url, self.level)
-            self.return_url, self.return_level = await self.handler.new_urls.get()
+            self.return_url, self.return_level, self.return_try_count = (await self.handler.new_urls.get()).values()
+
+        if not self.handler:
+            raise Exception("Error initializing Cloner!")
 
         self.loop.run_until_complete(test())
         self.assertEqual(self.return_content, "/foo/путь/")
+        if not self.return_url:
+            raise Exception("URL returned is None!")
         self.assertEqual(yarl.URL(self.return_url).human_repr(), self.expected_content)
         self.assertEqual(self.return_level, self.level + 1)
+        self.assertLess(self.return_try_count, 2)
 
         self.handler.moved_root = yarl.URL("http://example2.com")
         self.expected_content = "http://example2.com/foo/путь/"
@@ -65,19 +76,24 @@ class TestProcessLinks(unittest.TestCase):
         self.expected_content = ""
 
         async def test():
+            if not self.handler:
+                raise Exception("Error initializing Cloner!")
             self.return_content = await self.handler.process_link(self.url, self.level)
-            self.return_url, self.return_level = await self.handler.new_urls.get()
+            self.return_url, self.return_level, self.return_try_count = (await self.handler.new_urls.get()).values()
 
         self.loop.run_until_complete(test())
         self.assertEqual(self.return_content, self.expected_content)
         self.assertEqual(yarl.URL(self.url), self.return_url)
         self.assertEqual(self.return_level, self.level + 1)
+        self.assertLess(self.return_try_count, 2)
 
     def test_check_host(self):
         self.url = "http://foo.com"
         self.return_size = 0
 
         async def test():
+            if not self.handler:
+                raise Exception("Error initializing Cloner!")
             self.return_content = await self.handler.process_link(self.url, self.level, check_host=True)
             self.qsize = self.handler.new_urls.qsize()
 
@@ -91,6 +107,8 @@ class TestProcessLinks(unittest.TestCase):
         yarl.URL = mock.Mock(side_effect=UnicodeError)
 
         async def test():
+            if not self.handler:
+                raise Exception("Error initializing Cloner!")
             self.return_content = await self.handler.process_link(self.root, self.level)
 
         self.loop.run_until_complete(test())
